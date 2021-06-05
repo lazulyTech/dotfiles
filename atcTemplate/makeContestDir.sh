caa4e41cfd71c41d354df1dbea866d41691fbb65
Lazuly <41747902+lazulyTech@users.noreply.github.com>
2021-06-05 21:15:33 +0900

    Add AtCoder initialize files

---
+++ atcTemplate/makeContestDir.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+mkdir ${1}
+cd ${1}
+for var in a b c d e f
+do
+	cat ~/.dotfiles/atcTemplate/template.cpp > $var.cpp
+done
+mkdir cmake-build-debug
+cat ~/.dotfiles/atcTemplate/CMakeLists.txt > CMakeLists.txt
+cd cmake-build-debug
+cmake ..
+make
+ln -s compile_commands.json ../compile_commands.json
+
---
+++ atcTemplate/template.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <cmath>
+#include <iterator>
+#include <queue>
+#define ll long long
+
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
