e19811195a5f5ef378ceb4e6054437aec26c11a5
Lazuly <41747902+lazulyTech@users.noreply.github.com>
2021-07-04 23:48:36 +0900

    zshのaliasを変更

--- .zsh/00-options.zsh
+++ .zsh/00-options.zsh
@@ -7,9 +7,6 @@ SAVEHIST=1000000
 setopt inc_append_history
 setopt share_history
 
-## パスを直接入力してもcdする
-setopt AUTO_CD
-
 ## 環境変数を補完
 setopt AUTO_PARAM_KEYS
 
--- .zsh/20-alias.zsh
+++ .zsh/20-alias.zsh
@@ -1,5 +1,18 @@
 alias jxa="osascript -l JavaScript"
 alias ntex="nvim *.tex"
 alias cl21tex="platex *.tex ; dvipdfmx -V4 *.dvi"
-alias ls="ls -l"
+alias ls="ls -Gl"
+alias d="~/dotfiles"
+
+#git alias
+alias g='git'
+alias ga='git add'
+alias gd='git diff'
+alias gs='git status'
+alias gp='git push'
+alias gb='git branch'
+alias gst='git status'
+alias gco='git checkout'
+alias gf='git fetch'
+alias gc='git commit'
 
